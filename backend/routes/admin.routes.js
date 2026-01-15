cce698ddb1739910333a8762df98e889d9f78e0d
Aman Singh <amankrsingh58@gmail.com>
2026-01-15 19:53:51 +0530

    file name cchanges

--- backend/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getUsers } = require("../controllers/admin.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
-const roleMiddleware = require("../middlewares/role.middleware");
-const router = express.Router();
-
-router.get("/users", authMiddleware, roleMiddleware("admin"), getUsers);
-module.exports = router;
